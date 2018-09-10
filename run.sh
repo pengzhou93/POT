4ae28f00ecedaf257b6786088a607578dd8000a4
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2018-09-10 17:13:34 +0800

    debug plot_OT_1D.py

--- run.sh
+++ run.sh
@@ -36,6 +36,11 @@ delete_debug_string()
     fi
 }
 
+rebuild_cython()
+{
+    rm -rf build
+    python setup.py build_ext --inplace
+}
 
 # python3.6 tf_1_6
 source $HOME/anaconda3/bin/activate tf_1_6
@@ -44,24 +49,36 @@ export LD_LIBRARY_PATH=/usr/local/cudnn-9.0-v7.1/lib64:/usr/local/cuda-9.0/lib64
 if [ "$1" = build ]
 then
 #   ./run.sh build
-    python setup.py build_ext --inplace
+    rebuild_cython
 
 elif [ "$1" = "notebooks/plot_OT_1D.ipynb" ]
 then
     # ./run.sh "notebooks/plot_OT_1D.ipynb" debug
+    # ./run.sh "notebooks/plot_OT_1D.ipynb" debug-cpp
 
     file="plot_OT_1D.py"
     line=22
     if [ $2 = debug ]
     then
+        rebuild_cython
         cd notebooks
         insert_debug_string "$file" $line "$debug_str"
         python "$file"
         delete_debug_string "$file" $line "$debug_str"
-    else
-        jupyter notebook --browser google-chrome
-    fi
+    elif [ $2 = debug-cpp ]
+    then
+        rebuild_cython
+        cd notebooks
+#        gdbserver localhost:8080 python $file
 
+    elif [ $2 = jupyter ]
+    then
+        jupyter notebook --browser google-chrome
+
+    else
+        cd notebooks
+        python $file
+    fi
 
 else
     echo NoParameter
